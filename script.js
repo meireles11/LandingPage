35014a0e14a774665d5187377c6f5d3eda104609
meireles11 <miguel.meireles.cardoso@gmail.com>
2025-08-09 13:22:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,18 +4,16 @@ const mensagem = document.getElementById("mensagem");
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  const dados = new URLSearchParams();
-  for (const pair of new FormData(form)) {
-    dados.append(pair[0], pair[1]);
-  }
+  // Converter o formulário em URLSearchParams para evitar preflight CORS
+  const dados = new URLSearchParams(new FormData(form));
 
   fetch("https://script.google.com/macros/s/AKfycby6VNl_isEJF3qs4W-K3-14Qc7l83itQRnYtT8-HLiXqFztmzPd68iTDiD07yPqgzWC/exec", {
     method: "POST",
     body: dados,
     headers: { "Content-Type": "application/x-www-form-urlencoded" }
   })
-    .then((response) => response.json())
-    .then((data) => {
+    .then(response => response.json())
+    .then(data => {
       if (data.status === "success") {
         form.style.display = "none";
         mensagem.style.display = "block";
@@ -23,9 +21,8 @@ form.addEventListener("submit", function (e) {
         alert("Erro ao enviar o formulário.");
       }
     })
-    .catch((error) => {
+    .catch(error => {
       console.error("Erro:", error);
       alert("Erro ao enviar o formulário.");
     });
 });
-
